743bec7cf8a8ecf0777e24df796a810632124bf6
Mike Ball <mike@snoogen.com>
2015-12-03 16:38:39 -0700

    Update spec_helper.rb
    Automatically include all .rb files in the /lib directory.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,6 @@ RSpec.configure do |config|
   config.order = 'random'
 end
 
-# Dir[File.join(File.dirname(__FILE__), "lib" , "**.rb")].each do |f| 
-#   require f
-# end
+Dir[File.join(File.dirname(__FILE__), "lib" , "**.rb")].each do |f| 
+ require f
+end
